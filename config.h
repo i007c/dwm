b559ef06e830fa7e90e66de37a1ca9fc5fc3b6f4
007 <dr007cc@gmail.com>
2022-08-19 20:47:03 +0430

    fix: bigger font size bor bar

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int snap = 32 * 2; /* snap pixel */
 static const int showbar = 1;        /* 0 means no bar */
 static const int topbar = 1;         /* 0 means bottom bar */
 
-static const char *fonts[] = { "monospace:size=11" };
+static const char *fonts[] = { "monospace:size=12" };
 static const char dmenufont[] = "monospace:size=14";
 
 static const char *colors[][3] = {
